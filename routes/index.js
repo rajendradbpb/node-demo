f9412d5054725ace704b4bf256b0a2301c763bcb
rajendradbpb <rajendrasahoodbpb@gmail.com>
2021-01-17 21:13:27 +0530

    - updae

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,14 @@ var express = require('express');
 var router = express.Router();
 var usersRouter = require("./users")
 var loggerRouter = require("./logger-route")
+var promiseRouter = require("./promise-route")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.use('/logger', loggerRouter);
+router.use('/promise', promiseRouter);
 router.use('/users', usersRouter);
 
 module.exports = router;
